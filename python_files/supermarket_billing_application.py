7ceebff637c07fc068accfddd07ec7f20c8a2150
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-01-12 23:28:32 +0530

    Refactor billing application with product retrieval
    Updated database connection details and added product retrieval functionality. Enhanced billing function to include customer name and total payable calculation.

--- python_files/supermarket_billing_application.py
+++ python_files/supermarket_billing_application.py
@@ -1,10 +1,10 @@
 import mysql.connector
 
 
-conn_obj = mysql.connector.connect(host="localhost", user="root", password="ENTER YOUR PASSWORD", database="ENTER YOUR DATABASE NAME") # to connect with MySql
+conn_obj = mysql.connector.connect(host="localhost", user="root", password="Arghya@123", database="Billing_Application") # to connect with MySql
 cur_obj = conn_obj.cursor() # to execute the sql qeuries, hit SQL
 
-# To retrieve user data based on Phone Number
+# To retrieve user data based on Phone Number from Customer Details Table
 def data_retrieve(ph_no):
     query = f"select * from cust_details where c_ph_no = {ph_no}"
     result = None
@@ -18,7 +18,7 @@ def data_retrieve(ph_no):
     return result
 
 
-# To Create new user in the Database
+# To Create new user in the Customer Details table in Database
 def customer_entry(cust_full_name, cust_address, cust_phone_num):
     query = f"insert into cust_details (c_full_name, c_address, c_ph_no) values (%s, %s, %s);"
     data = (cust_full_name, cust_address, cust_phone_num)
@@ -31,21 +31,59 @@ def customer_entry(cust_full_name, cust_address, cust_phone_num):
         print("Error retrieving data from MySQL:", e)
         conn_obj.rollback()
 
+# To retrieve product details based on product ID from Product Details Table
+def product_details_retrieve(pid):
+    query = f"select * from p_details where pid = {pid}"
+    result = None
+    try:
+        cur_obj.execute(query) # To run the given query
+        result = cur_obj.fetchone()  # limit 1 in SQL
+        conn_obj.commit()
+    except mysql.connector.Error as e:
+        print("Error retrieving data from MySQL:", e)
+        conn_obj.rollback()
+    return result
+
 
 # For the Billing of the user
-def billing_function():
-    pass
+def billing_function(customer_name):
+    print("Starting the billing.")
+    total_payble = 0
+    count = 0
+    stop_option = ""
+    while True:
+        if count>0:
+            stop_option = input("Press 's' Enter to stop billing. Otherwise press any other key to continue.")
+
+        if stop_option == "s":
+            break
+        else:
+            count += 1
+            pid = int(input("Enter the Product ID: "))
+            p_details = product_details_retrieve(pid)
+            if p_details:
+                p_price = p_details[2]
+                p_name = p_details[1]
+                print(f"Product {p_name} has Price of  {p_price} ")
+                p_quantiy = int(input("Enter the Product Quantity Customer Brought: "))
+                total_price = p_price * p_quantiy
+                print("Total Price : ", total_price)
+                total_payble = total_payble + total_price
+                print("Total Payable: ", total_payble)
+            else:
+                print("No Product Details Found")
+
+    print(f"Hi {customer_name}! Your total bill amount : {total_payble}")
 
 
 def main():
-    print("Welcome to Star Supermarket")
     cust_phone_num = input("Enter Customer Phone Number: ")
     existing_user = data_retrieve(cust_phone_num)
 
     if existing_user:
         # For Existing user start billing
-        print("Customer Details Exist. Starting the billing.")
-        billing_function()
+        print(f"Hi {existing_user[1]}, Welcome to Star Super Market!!")
+        billing_function(existing_user[1])
     else:
         # Register User for Non Existing user
         print("To start billing we need to register your Mobile Number")
