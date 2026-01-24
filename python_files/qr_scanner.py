424b1c6c8c50b1d376df9fa30ee6e8c26696b9b7
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-01-25 00:50:24 +0530

    feat: add QR code support
    Cashier can now scan the Product QR Directly with out any manual entry.

---
+++ python_files/qr_scanner.py
@@ -0,0 +1,29 @@
+import cv2
+
+def qr_code_scanner():
+    cap = cv2.VideoCapture(0)
+    detector = cv2.QRCodeDetector()
+
+    print("Scanning QR... Press 'q' to exit")
+
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            continue
+
+        data, bbox, _ = detector.detectAndDecode(frame)
+
+        cv2.imshow("QR Code Scanner", frame)
+
+        if data:
+            cap.release()
+            cv2.destroyAllWindows()
+            print(data.strip())
+            return data.strip()
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+
+    cap.release()
+    cv2.destroyAllWindows()
+    return None
