ee1b685beeb0801db3349f71b0722862c17b7040
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-02-04 14:10:35 +0530

    Update Analytics Table.sql

--- sql_files/Analytics Table.sql
+++ sql_files/Analytics Table.sql
@@ -3,7 +3,7 @@ CREATE TABLE analytics_table (
     bill_id INT AUTO_INCREMENT PRIMARY KEY,
     c_id INT not null,
     c_name VARCHAR(100) not null,
-    c_ph_no bigint not null,
+    c_ph_no VARCHAR(10) not null,
     total_bill_value DECIMAL(10,2) not null,
     total_amount_payble_after_tax DECIMAL(10,2) not null,
     timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
