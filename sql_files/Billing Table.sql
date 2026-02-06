4fe6ed8c7fdf8f2eeef08a3312b7655c7cc99783
Arghyajyoti Samui <108090661+Arghyajyoti007@users.noreply.github.com>
2026-02-06 14:36:47 +0530

    Add primary key to billing_details table

--- sql_files/Billing Table.sql
+++ sql_files/Billing Table.sql
@@ -1,11 +1,13 @@
 -- Billing Details (Stores Every Single Product that Customer Brought)
-create table billing_details (
-	bill_id int not null,
-    cust_id int not null,
-    cust_name varchar(50) not null,
-    p_id int not null,
-    p_quantity int not null,
-    timestamp timestamp default current_timestamp
-); 
+-- security and performance rule: every table must have a Primary Key
+CREATE TABLE billing_details (
+    entry_id INT AUTO_INCREMENT PRIMARY KEY,
+    bill_id INT NOT NULL,
+    cust_id INT NOT NULL,
+    cust_name VARCHAR(50) NOT NULL,
+    p_id INT NOT NULL,
+    p_quantity INT NOT NULL,
+    timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 select * from billing_details;
